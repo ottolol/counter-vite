ce051f0de45353a04598c26592c3cde6a97b104d
ottolol <alexsveshnik@gmail.com>
2025-03-02 22:19:23 +0300

    add settings

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -5,6 +5,7 @@ import { S } from "./Counter_Styles";
 
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const [settings, setSettings] = useState(false);
 
   const IncHandlerClick = () => {
     setCounter(counter + 1);
@@ -17,34 +18,52 @@ export const Counter = () => {
   const maxCount = 5;
   const maxCountRes = counter === maxCount;
 
+  const SetHandlerClick = () => {
+    setSettings(!settings);
+  };
+
   return (
     <S.Wrapper>
       <S.Container>
-        <S.Border>
-          <S.CounterBody>
-            <span
-              style={{
-                color: maxCountRes ? "red" : `${theme.colors.primayBg}`,
-              }}
-            >
-              {counter}
-            </span>
-          </S.CounterBody>
-          <S.WrapperButtons>
-            <Button
-              isDisabled={maxCountRes ? true : false}
-              onClick={IncHandlerClick}
-            >
-              inc
-            </Button>
-            <Button
-              isDisabled={counter > 0 ? false : true}
-              onClick={ResetHandlerClick}
-            >
-              reset
-            </Button>
-          </S.WrapperButtons>
-        </S.Border>
+        {!settings ? (
+          <S.Border>
+            <S.CounterBody>
+              <span
+                style={{
+                  color: maxCountRes ? "red" : `${theme.colors.primayBg}`,
+                }}
+              >
+                {counter}
+              </span>
+            </S.CounterBody>
+            <S.WrapperButtons>
+              <Button
+                isDisabled={maxCountRes ? true : false}
+                onClick={IncHandlerClick}
+              >
+                inc
+              </Button>
+              <Button
+                isDisabled={counter > 0 ? false : true}
+                onClick={ResetHandlerClick}
+              >
+                reset
+              </Button>
+              <Button onClick={SetHandlerClick}>set</Button>
+            </S.WrapperButtons>
+          </S.Border>
+        ) : (
+          <S.Border>
+            <S.CounterBody>
+              <div>Max Value:</div>
+              <div>Start Value:</div>
+              <span>123</span>
+            </S.CounterBody>
+            <S.WrapperButtons style={{ textAlign: "center" }}>
+              <Button onClick={SetHandlerClick}>set</Button>
+            </S.WrapperButtons>
+          </S.Border>
+        )}
       </S.Container>
     </S.Wrapper>
   );
