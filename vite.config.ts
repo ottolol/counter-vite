8261fa112d89686e2e486bc65420ba3335f20a60
ottolol <alexsveshnik@gmail.com>
2025-03-02 21:48:39 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-styled-components"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    outDir: "build", // Устанавливаем директорию сборки на 'build'
+  },
+  base: "/your-repo-name/", // Установите базовый путь для GitHub Pages
+});
