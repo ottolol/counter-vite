2827a7a21350b2fc4639431a81e98ebfdb86369b
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-03-26 13:10:25 +0000

    пофиксил localstorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,52 @@
+// store.ts
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "../model/counter-reducer";
-// import { tasksReducer } from "../model/tasks-reducer";
-// import { todolistsReducer } from "../model/todolists-reducer";
 
+// Функция для загрузки состояния из localStorage
+const loadStateFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined; // Если данных нет, возвращаем undefined
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error("Ошибка загрузки состояния из localStorage:", err);
+    return undefined;
+  }
+};
+
+// Инициализация состояния из localStorage
+const preloadedState = loadStateFromLocalStorage();
 
 // объединение reducer'ов с помощью combineReducers
 const rootReducer = combineReducers({
-//   tasks: tasksReducer,
-//   todolists: todolistsReducer,
   counter: counterReducer,
 });
 
 // создание store
 export const store = configureStore({
   reducer: rootReducer,
+  preloadedState, // Передаем предзагруженное состояние
 });
 
 // автоматическое определение типа всего объекта состояния
 export type RootState = ReturnType<typeof store.getState>;
+
 // автоматическое определение типа метода dispatch
 export type AppDispatch = typeof store.dispatch;
+
+
+// Сохранение состояния в localStorage при изменении
+store.subscribe(() => {
+  try {
+    const state = store.getState();
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    console.error("Ошибка сохранения состояния в localStorage:", err);
+  }
+});
 
 // для возможности обращения к store в консоли браузера
 // @ts-ignore
